377728c3c89f62d48d4aa035a27af9cd7817fad7
Martín García <martin@behemot.local>
2020-10-30 17:35:13 +0100

    best header in the world

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,20 +1,38 @@
-#include "../includes/builtins.h"
-#include "../includes/libft.h"
+#include "builtins.h"
+#include "tree.h"
 
 int main(int v, char **n, char **envp)
 {
 	int	i;
 	char	*line;
+	t_list	*tree_list;
 
 	if (n != 0)
 	{} //error
+	
+	ft_printf("***************************************************************\n");
+	ft_printf("*                                                             *\n");
+	ft_printf("*                                             _ _       \\ \\   *\n");
+	ft_printf("*      --    MINISHELL v0.0  --    .-\"\"\"\"\"-. / \\_> /\\    |/   *\n");
+	ft_printf("*                                 /         \\.'`  `',.--//    *\n");
+	ft_printf("*                               -(    Y2K    I      I  @@\\    *\n");
+	ft_printf("*   The only bug in our code ->   \\         /'.____.'\\___|    *\n");
+	ft_printf("*                                  '-.....-' __/ | \\   (`)    *\n");
+	ft_printf("*                                           /   /  /          *\n");
+	ft_printf("*      by mgarcia- & daprovin                    \\  \\         *\n");
+	ft_printf("*                                                             *\n");
+	ft_printf("***************************************************************\n");
+
 	init_env(envp);
 	i = 1;
 	while(n)
 	{
 		ft_printf("(los voltereta)-> ");
 		i = gnl(0, &line);
+		tree_list = parse_line(line);
+		draw_list(tree_list);
 		//do whatever with line
 		free(line);
 	}
+	return (0);
 }
