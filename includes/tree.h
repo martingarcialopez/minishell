ef2962dc489a99729fa33eb47b0ccc44d8a3ed3c
Martín García <martin@behemot.local>
2020-10-30 10:12:22 +0100

    ordenan2

--- includes/tree.h
+++ includes/tree.h
@@ -13,19 +13,21 @@
 #ifndef _TREE_H_
 # define _TREE_H_
 
+#include "libft.h"
+
 typedef struct		s_tree
 {
-	char			**data;
+	char		**data;
 	struct s_tree	*left;
 	struct s_tree	*right;
-}					t_tree;
+}			t_tree;
 
 
 t_tree			*ft_newtree(char **content);
 
-void			ft_treadd_root(t_tree **rtree, t_tree *new);
+void			ft_treeadd_root(t_tree **rtree, t_tree *new);
 
-int				ft_add_leaf_dfs(t_tree **rtree, t_tree *new);
+int			ft_add_leaf_dfs(t_tree **rtree, t_tree *new);
 
 void			print_dfs_tree(t_tree *node);
 
