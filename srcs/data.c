cf25c5154cf67b06e194236704a2d25d82accc19
daprovin <daprovin@student.42.fr>
2020-11-11 20:41:57 +0100

    g_data included and working

---
+++ srcs/data.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+#include "builtins.h"
+#include "tree.h"
+
+
+int	init_data(void)
+{
+	t_env	*list;
+
+	list = g_env;
+	while (list != NULL)
+	{
+		if (ft_strcmp(list->name, "PWD") == 0)
+			g_data[PWD] = ft_strdup(list->value);
+		else if (ft_strcmp(list->name, "OLDPWD") == 0)
+			g_data[OLDPWD] = ft_strdup(list->value); //securizar
+		else if (ft_strcmp(list->name, "?") == 0)
+			g_data[RET] = ft_strdup(list->value); //securizar
+		list = list->next;
+	}
+	return (0);
+}
