273f5b4d68b966935785a4d3f0bcf03909d34c96
Martín García <martin@behemot.local>
2020-11-12 23:29:15 +0100

    retrieve_env_var updated with g_data[]

--- srcs/utils.c
+++ srcs/utils.c
@@ -10,6 +10,16 @@ int	retrieve_env_variable(char *name, char **value)
 	t_env	*list;
 	
 	list = g_env;
+	if (ft_strcmp("PWD", name) == 0)
+	{
+		*value = g_data[PWD];
+		return (1);
+	}
+	else if (ft_strcmp("RET", name) == 0)
+	{
+		*value = g_data[RET];
+		return (1);
+	}
 	while (list)
 	{
 		if (ft_strcmp(list->name, name) == 0)
