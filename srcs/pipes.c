20bb1cbfc71be344205941685b7600079358fdad
Martín García <martin@behemot.local>
2020-11-17 13:16:46 +0100

    pipeezz workiiinnnn (again)

--- srcs/pipes.c
+++ srcs/pipes.c
@@ -1,16 +1,6 @@
 #include "builtins.h"
 #include "tree.h"
 
-static int		is_operator(char *str)
-{
-	if (ft_strcmp(str, "|") == 0 || ft_strcmp(str, "||") == 0
-		|| ft_strcmp(str, "&&") == 0 || ft_strcmp(str, ">") == 0
-		|| ft_strcmp(str, "<") == 0 || ft_strcmp(str, "<<") == 0
-		|| ft_strcmp(str, ">>") == 0)
-		return (1);
-	return (0);
-}
-
 void		pipes(t_tree *tree, int count, int backup_fd)
 {
 	int	fd[2];
@@ -22,41 +12,35 @@ void		pipes(t_tree *tree, int count, int backup_fd)
 	{
 		dup2(backup_fd, 0);
 		if (count)
-		{
 			dup2(fd[1], 1);
-		//close(fd[0]);
+		close(fd[0]);
+		if (count)
 			exec_commands(tree->left);
-		}
 		else
 			exec_commands(tree);
 		exit(0);
 	}
-	else if (fk > 0)
-	{
-		wait(NULL);
-		close(fd[1]);
-		if (count)
-			pipes(tree->right, count - 1, fd[0]);
-	}
+	close(fd[1]);
+	if (tree->right)
+		pipes(tree->right, count - 1, fd[0]);
+	wait(NULL);
 }
 
 int		exec_commands(t_tree *tree)
 {
-//	t_tree		*root;
 	int		ret;
 
-//	root = tree;
 	if (!tree)
 		return (0);
 	if (tree->type == right_redir || tree->type == left_redir
 		|| tree->type == double_right_redir)
 		redirection(tree);
-	else if (tree->type == pipeline)
-	{
-//		ft_printf("yo\n");
+	if (tree->type == pipeline)
 		pipes(tree, 1, 0);
-	}
-	else if (tree->type == literal)
+	if (tree->type == literal)
+	{
+		ft_printf("");
 		return (function(tree->data));
+	}
 	return (ret);
 }
