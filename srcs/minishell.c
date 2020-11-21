c87b8639be6c0356028c5646e1afaa10af61ff48
Martín <mgarcia-@student.42.fr>
2020-11-21 22:05:03 +0100

    remove slash + parameter redistribution

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -85,8 +85,6 @@ int		    exec_single_command(char *line)
 
 int		    main(int ac, char **av, char **envp)
 {
-	(void)ac;
-	(void)av;
 	if (ac > 1)
 	{
 	    if (ac >= 2 && ft_strcmp(av[1], "-c") == 0)
@@ -96,6 +94,9 @@ int		    main(int ac, char **av, char **envp)
 		    ft_printf_fd(2, "%s: -c: option requires an argument\n", g_data[ARGV0]);
 		    exit(1);
 		}
+		ft_printf("hello hello ac is %d\n", ac);
+		while (--ac > 2)
+		    av[ac - 3] = av[ac];
 		init_env(envp);
 		init_data(av);
 		return (exec_single_command(av[2]));
